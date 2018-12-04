77b8abca409c1cc6ba9c25f6bc2e65407308616d
QrityKim <bhj970@gmail.com>
2018-12-04 11:05:59 +0900

    to pc

--- src/accordion.js
+++ src/accordion.js
@@ -1,52 +1,95 @@
-$(function () {
-  "use strict";
 
-  class Accordion {
-    constructor(options) {
-      this.config = Accordion.mergeConfig(options);
-      this.$accordion = $(this.config.selector);
-      // jquery selector로 accordion을 선택한다.
-      this.init();
-      this.$accordion.addEventListener("click", this.toggle.bind(this));
-      // 이벤트 핸들러 내부의 this는 현재 가르키고있는 요소가 된다.
-    }
+  class Carousel {
+    constructor() {
+      this.carousel = document.querySelector(".gallery");
+      this.container = this.carousel.querySelector(".slide-container");
+      // this.item = $(".gallery .slides");
+      this.item = this.carousel.querySelector('.slides');
+      this.prev = this.carousel.querySelector(".prev");
+      this.next = this.carousel.querySelector(".next");
+      this.itemWidth = this.item.offsetWidth;
+      this.itemHeight = this.item.offsetHeight;
+      this.itemLength = this.carousel.querySelectorAll(".slides").length;
 
-    static mergeConfig(options) {
-      const config = {
-        selector: "#accordion",
-        multi: true
+      this.offset = 0;
+      this.currentItem = 1;
+
+      // 설정 정보
+      this.config = {
+        duration: 200,
+        easing: 'ease-out'
       };
 
-      return {...config, ...options};
+      this.init();
+      this.attachEvent();
     }
 
     init() {
-      const $ActiveSubmenu = this.$accordion.$(".active .submenu");
-      if ($ActiveSubmenu) $ActiveSubmenu.style.height = $ActiveSubmenu.scrollHeight + "px";
+      this.carousel.style.width = this.itemWidth + 'px';
+        this.carousel.style.height = this.itemHeight + 'px';
+
+        // carousel 요소의 width/height의 셋팅이 완료되면 carousel 요소를 보여준다.
+        this.carousel.style.opacity = 1;
+      this.checkMovable();
     }
 
-    toggle(event) {
-      if (!event.target.classList.constains("menu")) return;
+    attachEvent() {
+        this.prev.addEventListener('click', this.moveToPrev.bind(this));
+        this.next.addEventListener('click', this.moveToNext.bind(this));
+    }
 
-      const $targetItem = event.target.parentNode;
+    moveToPrev() {
+        // carousel-container 요소를 오른쪽으로 이동시키기 위해 이동거리를 캐러셀 아이템의 너비만큼 증시킨다.
+        this.offset += this.itemWidth;
+        // 이전 슬라이더로 이동하기 위해 carousel-container 요소를 오른쪽으로 이동시킨다.
+        this.move();
+        // 현재 표시 중인 캐러셀 아이템 인덱스(1~4)
+        this.currentItem--;
+        // prev, next 버튼 활성화/비활성화 결정
+        this.checkMovable();
+    }
 
-      if (!this.config.multi) {
-        [].filter.call(
-          this.$accordion.childNodes,
-          li => li.nodeType === Node.ELEMENT_NODE && li !== $targetItem && li.classList.contains("active")
-        ).forEach(li => {
-          li.classList.remove("active");
-          li.querySelector(".submenu").style.height = "0";
-        });
+      /*
+        next 버튼 이벤트 핸들러
+        다음 슬라이더로 이동하기 위해 carousel-container 요소를 왼쪽으로 이동시킨다.
+      */
+      moveToNext() {
+        // carousel-container 요소를 왼쪽으로 이동시키기 위해 이동거리를 캐러셀 아이템의 너비만큼 감소시킨다.
+        this.offset -= this.itemWidth;
+        // 다음 슬라이더로 이동하기 위해 carousel-container 요소를 왼쪽으로 이동시킨다.
+        this.move();
+        // 현재 표시 중인 캐러셀 아이템 인덱스(1~4)
+        this.currentItem++;
+        // prev, next 버튼 활성화/비활성화 결정
+        this.checkMovable();
       }
 
-      $targetItem.classList.toggle("active");
-      const $submenu = $targetItem.querySelector(".submenu");
-      $submenu.style.height = $target.classList.contains("active") ? $submentu.scrollHeight + "px" : "0";
-    }
+      // offset 만큼 carousel-container 요소를 이동시킨다.
+      move() {
+        this.container.style.transition = `transform ${this.config.duration}ms ${this.config.easing}`;
+        this.container.style.transform = `translate3D(${this.offset}px, 0, 0)`;
+      }
+
+      // prev, next 버튼 활성화/비활성화 결정
+      checkMovable() {
+        if (this.currentItem === 1) {
+          this.prev.disabled = true;
+          this.prev.classList.add('disabled');
+        } else {
+          this.prev.disabled = false;
+          this.prev.classList.remove('disabled');
+        }
+
+        if (this.currentItem === this.itemLength) {
+          this.next.disabled = true;
+          this.next.classList.add('disabled');
+        } else {
+          this.next.disabled = false;
+          this.next.classList.remove('disabled');
+        }
+      }
   }
 
   window.onload = function () {
-    const accordion = new Accordion({multi: false});
+    const carousel = new Carousel();
   };
-});
